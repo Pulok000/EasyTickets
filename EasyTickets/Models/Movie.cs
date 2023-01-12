6eb32d9aefca90e9d7848477011c6e5fdaa90e5f
Pulok Ahmed <pulokahmed07@gmail.com>
2023-01-12 17:47:57 +0600

    1: Model:Upto ActorMovies

---
+++ EasyTickets/Models/Movie.cs
@@ -0,0 +1,29 @@
+﻿using EasyTickets.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyTickets.Models
+{
+    public class Movie
+    {
+
+        [Key]
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+        public string ImageURL { get; set; }
+
+        public string StartDate { get; set; }
+
+        public string EndDate { get; set; }
+
+        public MovieCatagory MovieCatagory { get; set; }
+    }   
+}
